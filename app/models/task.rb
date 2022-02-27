635661935c81c9319eb3814c9c062b5459f8d5ef
remi-1201 <remi.nkjm@gmail.com>
2022-02-27 18:28:35 +0900

    バリデーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,7 @@ class Task < ApplicationRecord
   has_many :labels, through: :labelings
   accepts_nested_attributes_for :labels, reject_if: :all_blank, allow_destroy: true
   belongs_to :user
+
+  validates :name, presence: true, length: {maximum: 120}
+  validates :details, presence: true, length: {maximum: 1000}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :labels, dependent: :destroy
+
+  has_secure_password
+  before_validation { email.downcase! }
+  validates :name,  presence: true, length: { maximum: 120 }
+  validates :email, presence: true, length: { maximum: 250 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+
 end
