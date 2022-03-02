031b743fdf2fa24e935d2e6a043bc8026ac988cf
remi-1201 <remi.nkjm@gmail.com>
2022-03-02 15:42:07 +0900

    アプリの国際化

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 module TaskApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
     config.load_defaults 6.0
     config.generators do |g|
       g.test_framework :rspec,
