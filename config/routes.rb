294552345de30d0220c685c03e9ae48f5ff6bf81
remi-1201 <remi.nkjm@gmail.com>
2022-03-05 21:59:36 +0900

    管理画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
     root 'tasks#index'
     resources :sessions, only: [:new, :create, :destroy]
     resources :users, only: [:new, :create, :show, :edit, :update, :index]
+    namespace :admin do
+      resources :users, except: [:show]
+    end
     resources :tasks do
       collection do
         post :sort
