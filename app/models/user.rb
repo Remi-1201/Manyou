d8a01049a6eb6bc754774a84c261293a9df8a1f0
remi-1201 <remi.nkjm@gmail.com>
2022-03-06 20:40:42 +0900

    最低一人の管理者を残す

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,25 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :labels, dependent: :destroy
+  before_destroy :destroy_action
+  before_update :update_action
 
   has_secure_password
   before_validation { email.downcase! }
   validates :name,  presence: true, length: { maximum: 120 }
   validates :email, presence: true, length: { maximum: 250 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+
+  private
+  def destroy_action
+    if User.where(admin: true).count == 1
+      throw(:abort)
+    end
+  end
+
+  def update_action
+    if User.where(admin: true).count == 1 && self.admin == false
+      errors.add(:admin, 'から外せません。最低一人の管理者が必要です')
+      throw(:abort)
+    end
+  end
 end
